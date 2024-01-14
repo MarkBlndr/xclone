19275d5c304c0508844dfba9ea7ecc31c25a492e
MarkBlndr <markblndr@gmail.com>
2024-01-14 16:53:35 -0500

    Added basic log in/out functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import Head from "next/head";
 
+import { SideNav } from "~/components/SideNav";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -12,7 +14,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>X Clone</title>
+        <meta
+          name="description"
+          content="This is an X (Twitter) clone by Mark Blinder"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
